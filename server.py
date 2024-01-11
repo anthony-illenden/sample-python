bbb34caa1f516f96973a98eeb872cd6247fba03f
Tony Illenden <130492486+anthony-illenden@users.noreply.github.com>
2024-01-11 16:11:23 -0500

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'UP UP and AWAY!! SUPER WASHED Ain't He??!! 🤣🤣🤣🤣🤷🏾‍♂️. Stay low and keep firing! The air up there is a tad bit different. LIVE.LAUGH.LOVE#striveforgreatness🚀 #thekidfromakron👑#jamesgang👑 #bronknows %s' % (self.path)
+        msg = 'UP UP and AWAY!! SUPER WASHED Ain't He??!! Stay low and keep firing! The air up there is a tad bit different. LIVE.LAUGH.LOVE#striveforgreatness #thekidfromakron#jamesgang #bronknows %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
